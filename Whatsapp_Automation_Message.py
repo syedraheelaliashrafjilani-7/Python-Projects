d0242108f36bdd7236f3f9b9d464e33edc957fce
Syed Raheel Ali <syedraheelaliashrafjilani@gmail.com>
2026-02-11 23:44:38 +0500

    Add files via upload

---
+++ Whatsapp_Automation_Message.py
@@ -0,0 +1,5 @@
+import pywhatkit as pwk
+
+phone = "123456789" #Enter Your Number To Send Message Automatically 
+message = "Hello .....XYZ" #Enter The Message You Want to send 
+pwk.sendwhatmsg_instantly(phone, message, wait_time=10) 
